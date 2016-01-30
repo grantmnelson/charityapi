748eb8b01e178358d65d7edd90c3eae0439ead20
Grant Nelson <grantmnelsn@gmail.com>
2016-01-30 15:46:16 -0500

    Added updating column on organization automatically

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -42,4 +42,13 @@ class Organization < ActiveRecord::Base
    end
  end
 
+ # This method checks if the field has a corresponding field_last_checked
+ def log_changed
+  self.attribute_names.each do |attribute_name|
+    if self.changes.include?(attribute_name) && Organization.column_names.include?("#{attribute_name}_last_changed")
+      self.update_attribute("#{attribute_name}_last_changed", Time.now)
+    end
+  end
+ end
+
 end
