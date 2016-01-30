d942a21d51b4cbebe69e5fe9fdf3447844e6eba5
Yusuf Ameri <yusuf.americianaki@gmail.com>
2016-01-30 09:33:36 -0500

    make model for 990-n postcard

---
+++ db/migrate/20160130132149_create_postcards.rb
@@ -0,0 +1,36 @@
+class CreatePostcards < ActiveRecord::Migration
+  def change
+    create_table :postcards do |t|
+      t.string :ein
+      t.integer :tax_year
+      t.string :organization_name
+      t.string :gross_reciepts_under_25k
+      t.boolean :terminated
+      t.date :tax_period_begin_date
+      t.date :tax_period_end_date
+      t.string :website_url
+      t.string :officer_name
+      t.string :officer_address_line_1
+      t.string :officer_address_line_2
+      t.string :officer_address_city
+      t.string :officer_address_province
+      t.references :officer_address_state, index: true
+      t.string :officer_address_postal_code
+      t.string :officer_address_country
+      t.string :organization_address_line_1
+      t.string :organization_address_line_2
+      t.string :organization_address_city
+      t.string :organization_address_province
+      t.references :organization_address_state, index: true
+      t.string :organization_address_postal_code
+      t.string :organization_address_country
+      t.string :doing_business_as_name_1
+      t.string :doing_business_as
+      t.string :name_2
+      t.string :doing_business_as_name_3
+      t.references :organization, index: true
+
+      t.timestamps null: false
+    end
+  end
+end
