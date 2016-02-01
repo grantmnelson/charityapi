2d275b6787c95c77856a7d667c0642be2a067d98
Grant Nelson <grantmnelsn@gmail.com>
2016-02-01 01:05:02 -0500

    test specs not right

--- test/integration/v1/base_controller_test.rb
+++ test/integration/v1/base_controller_test.rb
@@ -3,15 +3,7 @@ require 'test_helper'
 class V1::BaseControllerTest < ActionDispatch::IntegrationTest
   # Replace this with your real tests.
   test "the truth" do
-    # get(:eins, {ein: "000003154"}, {'Accept' => 'application/json'})
-
-  end
-end
-
-class EinsController < V1::BaseControllerTest
-  # Replace this with your real tests.
-  test "the yjhb" do
-    get(:eins, {ein: "000003154"}, {'Accept' => 'application/json'})
-
+    get('/eins', {ein: "000003154"}, {'Accept' => 'application/json'})
+    
   end
 end
