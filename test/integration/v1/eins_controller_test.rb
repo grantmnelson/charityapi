d003b84863b0d7b29ed0ff55b6d5a295696d51f8
Grant Nelson <grantmnelsn@gmail.com>
2016-02-04 21:38:24 -0500

    testing structure created!

--- test/integration/v1/eins_controller_test.rb
+++ test/integration/v1/eins_controller_test.rb
@@ -1,15 +1,25 @@
 require 'test_helper'
 
-class ApiTestV1 < ActionDispatch::IntegrationTest
+class APIV1EinsControllerTest < ActionDispatch::IntegrationTest
   setup do
-
+    @version = 1
+    @controller_route = "eins"
   end
 
-  test "returns X" do
-    get("/1/eins/000003154")
+  test "returns Data about Oakleaf" do
+    get("/#{@version}/#{@controller_route}/000003154")
+    @response = JSON.parse(response.body)["response"]
+    assert @response[:ein] = "000003154", "#{@response[:ein]}"
+  end
+
+
+
+
+  # Test Error States
+  test "returns nil if not found" do
+    get("/#{@version}/#{@controller_route}/no_value_should_return_nothing")
     assert_response :success
-    puts response.body
+    assert JSON.parse(response.body)[:response] = "null"
   end
-
 
 end
